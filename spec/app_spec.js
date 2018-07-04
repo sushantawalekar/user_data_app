a0a0e087e2badde092591830efdb032a1e543322
Olaf Kwant <olaf+github@zendesk.com>
2018-07-04 11:39:23 +1000

    new tests for app

--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,12 +1,92 @@
 /* eslint-env mocha */
 import app from '../src/javascript/app'
+import * as helpers from '../src/javascript/lib/helpers'
+import * as storage from '../src/javascript/lib/storage'
 import assert from 'assert'
+import sinon from 'sinon'
 
 describe('App', () => {
   describe('#toLocaleDate', function () {
     it('returns a string', function () {
       const result = app.toLocaleDate('2018-10-02T00:00:00Z')
       assert.strictEqual(result, '2/10/2018')
+    })
+  })
+
+  describe('#getLocales', () => {
+    before(() => {
+      sinon.stub(helpers, 'ajax').callsFake(() => {
+        return Promise.resolve({
+          locales: [
+            { id: 1005, locale: "nl", name: "Nederlands (Dutch)" },
+            { id: 1, locale: "en-US", name: "English" }
+          ]
+        })
+      })
+    })
+
+    after(() => {
+      helpers.ajax.restore()
+    })
+
+    it('returns a locales', (done) => {
+      app.getLocales().then((data) => {
+        assert.deepStrictEqual(data, {
+          "nl": "Nederlands (Dutch)",
+          "en-US": "English"
+        })
+        done()
+      })
+    })
+
+    it('stores the locales in storage', (done) => {
+      app.getLocales().then(() => {
+        assert.deepStrictEqual(storage.storage('locales'), {
+          "nl": "Nederlands (Dutch)",
+          "en-US": "English"
+        })
+        done()
+      })
+    })
+  })
+
+  describe('#couldHideField', () => {
+    let hideEmptyFields
+    const field = {}
+
+    before(() => {
+      sinon.stub(storage, 'setting').callsFake(() => {
+        return hideEmptyFields
+      })
+    })
+
+    after(() => {
+      storage.setting.restore()
+    })
+
+    it('return false when setting is false', () => {
+      hideEmptyFields = false
+      assert.strictEqual(app.couldHideField(field), false)
+    })
+
+    it('return false when field has a value', () => {
+      hideEmptyFields = true
+      field.value = 'test'
+      assert.strictEqual(app.couldHideField(field), false)
+    })
+
+    it('return false when field is editable', () => {
+      hideEmptyFields = true
+      field.value = ''
+      field.editable = true
+      assert.strictEqual(app.couldHideField(field), false)
+    })
+
+    it('return true when field has a value', () => {
+      hideEmptyFields = true
+      field.value = ''
+      field.editable = false
+      assert.strictEqual(app.couldHideField(field), true)
     })
   })
 })
