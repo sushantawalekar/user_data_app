44fecd7db83a0bc3ae767912d894d37442128dc3
Olaf Kwant <olaf+github@zendesk.com>
2018-07-18 10:55:45 +1000

    small code change

--- webpack/stats.js
+++ webpack/stats.js
@@ -2,16 +2,14 @@ const exec = require('child_process').exec
 const fs = require('fs')
 const zlib = require('zlib')
 
-Promise.resolve().then(() => {
-  return new Promise((resolve, reject) => {
-    exec('npm run build -- --env.stats', (error, stdout, stderr) => {
-      if (error) reject(stderr)
-      resolve()
-    })
+new Promise((resolve, reject) => {
+  exec('npm run build -- --env.stats', (error, stdout, stderr) => {
+    if (error) reject(stderr)
+    resolve()
   })
 }).then(() => {
   exec('open ./dist/statistics.html')
-}).then((result) => {
+}).then(() => {
   return new Promise((resolve, reject) => {
     fs.readFile('./dist/assets/bundle.js', 'utf8', (err, data) => {
       if (err) reject(err)
